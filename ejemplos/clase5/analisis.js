8283a3309d6189b7a09d2f624b44b88698fc8c88
danoc93 <dortiz93@gmail.com>
2022-07-05 23:42:42 +0100

    Cambios

--- ejemplos/clase5/analisis.js
+++ ejemplos/clase5/analisis.js
@@ -1,3 +1,3 @@
-const ftc = require("fetch");
-const resultado = ftc("http://127.0.0.1:5000/datos");
-console.log(resultado);
+import fetch from "node-fetch";
+const respuesta = fetch("http://127.0.0.1:5000/datos");
+respuesta.then((r) => r.json().then((j) => console.log(j)));
